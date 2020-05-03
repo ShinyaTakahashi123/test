13fd8473d8ad26e5e165497f46a2a32c02a58e1f
Shinya Takahashi <st.83.53.abcd@gmail.com>
2020-05-03 15:41:34 +0900

    etc

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+    <hr />
+    <pre>[{{ now }}]</pre>
+    <router-link to="/other">Go To Other</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+      message: 'this is message.',
+      now: 'wait....'
+    }
+  },
+  created() {
+    setInterval(() => {
+      const d = new Date()
+      this.now = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
+    }, 1000)
+  }
+}
+</script>
+
+<style>
+.container {
+  padding: 5px 10px;
+}
+h1 {
+  font-size: 60pt;
+}
+p {
+  padding-top: 5px;
+  font-size: 20pt;
+}
+pre {
+  padding: 10pt;
+  font-size: 10pt;
+  background: #beff0d;
+}
+hr {
+  margin: 10pt 5pt;
+}
+</style>
